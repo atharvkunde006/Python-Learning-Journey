204c4eeffa28d8612e904bf7c88a883238e2c02a
Atharv Kunde <atharvkunde006@gmail.com>
2026-02-19 18:02:28 +0530

    Add files via upload

---
+++ break_statement.py
@@ -0,0 +1,16 @@
+#first program
+for i in range(1,101,1):
+    print(i,end=" ")
+    if(i==50):
+        break
+    else:
+        print("mississipi")
+        print("Thank you")
+
+#second program
+for i in range(12):
+    if(i==10):
+        break
+    print("5x",i+1,"=",5*(i+1))
+    print ("loop ko chodkar nikal gaya")
+
