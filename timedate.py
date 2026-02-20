3aacfed90be528a81a092885a15f8d326b5d760b
Atharv Kunde <atharvkunde006@gmail.com>
2026-02-20 18:41:45 +0530

    Add files via upload

---
+++ timedate.py
@@ -0,0 +1,11 @@
+import datetime
+now=datetime.datetime.now()
+print(now)
+
+import datetime
+today=datetime.date.today()
+print(today)
+
+import datetime
+now=datetime.datetime.now()
+print(now.strftime("%H:%M:%S"))
