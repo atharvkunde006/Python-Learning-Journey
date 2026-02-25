78e69b1430212889a921c88044904b4fd392f13c
Atharv Kunde <atharvkunde006@gmail.com>
2026-02-25 21:40:01 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,35 @@
+list=[1,2,3,4,5]
+print(list)
+
+list=["green","Red","yellow","blue"]
+print(list)
+
+a=["Atharv","aryan","shlok"]
+a[0]="Sarthak"
+print(a)
+
+a=("atharv","shubham")
+print(a)
+
+list=["atharv",1515,True,5.9]
+print(list)
+
+list=["RED","GREEN","YELLOW","BLUE","ORANGE"]
+print(list[-1])
+print(list[-2])
+print(list[-3])
+print(list[-4])
+print(list[-5])
+
+freinds=["Atharv","kaustubh","yash","devendra"]
+if "Atharv"in freinds:
+    print("atharv is a freind")
+else:
+    print("Atharv is not a freind")
+
+
+animals=["dog","cat","camal","cow","ox","rabit"]
+print(animals[0:3])
+print(animals[2:5])
+print(animals[:-1])
+print(animals[-2:-5])
