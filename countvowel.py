99f3edcbd7e7b651b81856c8199b92ab47b95880
Atharv Kunde <atharvkunde006@gmail.com>
2026-02-25 18:55:52 +0530

    Add files via upload

---
+++ countvowel.py
@@ -0,0 +1,9 @@
+a=input("enter a text:")
+i=0
+for i in a:
+    if i in "aeiou":
+      count=+1
+      print(count)
+
+
+
---
+++ sumofDigits.py
@@ -0,0 +1,15 @@
+list=[5,8,9,6]
+total=sum(list)
+print(total)
+
+list=[1,2,5]
+total=0
+for i in list:
+    total+=1
+    print(total)
+
+def list_sum(list):
+ total=0
+for i in list:
+ total+=i
+ print(list_sum[1,2,3,4])
