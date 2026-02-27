ce616ff17077e95e39f90d324a2868bd63ab5d38
Atharv Kunde <atharvkunde006@gmail.com>
2026-02-27 22:09:21 +0530

    initial commit

---
+++ Doc-string.py
@@ -0,0 +1,11 @@
+#
+def add(a,b):
+    """This function returns sum of the numbers"""
+    return a+b
+print(add(2,5))
+
+##
+def greet(name):
+    """This function print greeting message"""
+    print(f"Hello{name}")
+print(greet.__doc__)
