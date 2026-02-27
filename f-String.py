437f768f707043dc99762d4be76bdf2c8d4fcb21
Atharv Kunde <atharvkunde006@gmail.com>
2026-02-27 21:24:54 +0530

    Add files via upload

---
+++ f-String.py
@@ -0,0 +1,30 @@
+#
+a="Atharv"
+b=20
+print(f"My name is{a} and i am {b} years old")
+
+##
+a=2
+b=4
+print(f"The sum is:{a+b}")
+print(f"The multiplication is:{a*b}")
+print(f"The subtraction is:{a-b}")
+print(f"The division is:{a/b}")
+
+###
+price=99.5634
+print(f"price:{price:2f}")
+
+####
+list=[1,2,3,4]
+print(f"The list element are:{list}")
+print(f"first mark is{[1]}")
+
+####
+for i in range(1,10):
+ print(f"The elements are:{i}")
+
+ #####
+ x=10
+ y=20
+ print(f"{x=},{y=}")
