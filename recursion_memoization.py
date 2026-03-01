1194e86d45bdc84d2d394b86f041ba1aabd148ba
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-01 18:16:34 +0530

    Add files via upload

---
+++ recursion_memoization.py
@@ -0,0 +1,11 @@
+def fibonacci(n,memo={}):
+    if n in memo:
+        return memo[n]
+    if n==0:
+        return 0
+    if n==1:
+            return 1
+    memo[n]=fibonacci(n-1 ,memo) 
+    + fibonacci(n-2 , memo)
+    return memo[n]
+print(fibonacci(7))
