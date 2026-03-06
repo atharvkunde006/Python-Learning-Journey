8e3295922847e0f8aba5f919bcb4a7ebc91d74ac
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-06 22:49:49 +0530

    Add files via upload

---
+++ if_else_oneLine.py
@@ -0,0 +1,13 @@
+#
+a=10
+b=30
+print (a) if a>b else print(b)
+
+##
+a=330
+b=330
+print(a) if a>b else print("=") if a == b else print(b)
+
+###
+age=int(input("Enter your age:"))
+print("i am Adult") if (age>20) else print("i am tenager") if(age>50) else print("i am mhatar")
