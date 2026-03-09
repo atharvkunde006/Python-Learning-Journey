09a22cfbb894d3632ee32b258231539aa91d3102
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-09 22:04:26 +0530

    Add files via upload

---
+++ math_import.py
@@ -0,0 +1,28 @@
+#
+import math
+result=math.sqrt(9)
+print(result)
+
+##
+import math
+result=math.factorial(5)
+print(result)
+
+###
+import math
+result=math.ceil(4.3)
+print(result)
+
+####
+import math
+result=math.floor(4.9)
+print(result)
+
+#####
+import math
+result=math.radians(90)
+print(result)
+
+######
+import math
+result=math.degrees(80)
