76e5e22d9f6809ee431ea198053d8148ce4618e0
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-21 22:03:16 +0530

    Add files via upload

---
+++ opps-in-python/class.py
@@ -0,0 +1,47 @@
+#
+class Detaile:
+    name="Atharv"
+    age=20
+obj1=Detaile()
+print(obj1.name)
+print(obj1.age)
+
+##
+class Student:
+    name="Atharv"
+    age="king"
+    qulification="B.Tech CSE"
+    sem2=8.3
+    sem3=8.5
+obj=Student()
+print(obj.name)
+print(obj.age)
+print(obj.qulification)
+print(obj.sem2)
+print(obj.sem3)
+print(obj.sem2+obj.sem3)
+print(obj.name+obj.age)
+    
+###
+class add:
+    num=20
+    num2=30
+obj=add()
+print(obj.num+obj.num2)
+
+####
+class area:
+    length=5
+    width=10
+obj=area()
+print("The Area of Rectangle is :",obj.length*obj.width)
+
+#####
+class calculator:
+    a=10
+    b=5
+o=calculator()
+print("Addition is:",o.a+o.b)
+print("subtraction is:",o.a-o.b)
+print("multiplecation is:",o.a*o.b)
+print("divide is:",o.a/o.b)
---
+++ opps-in-python/classHardQ.py
@@ -0,0 +1,7 @@
+class student:
+    a=80
+    b=78
+    c=95
+    d=65
+obj=student()
+print((obj.a+obj.b+obj.c+obj.d)/4)
