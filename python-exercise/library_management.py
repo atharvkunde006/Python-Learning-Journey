893d45308f8c56698b351a237f934eb49f4838d4
Atharv Kunde <atharvkunde006@gmail.com>
2026-04-24 21:54:58 +0530

    Add files via upload

---
+++ python-exercise/library_management.py
@@ -0,0 +1,24 @@
+print("Welcome to the Library")
+
+
+class Library:
+    def __init__(self):
+        self.nobooks = 0
+        self.books = []
+
+    def addbook(self, book):
+        self.books.append(book)
+        self.nobooks = len(self.books)
+
+    def show(self):
+        print(f"The library has {self.nobooks} books. The books are:")
+        for book in self.books:
+            print(book)
+
+
+l1 = Library()
+l1.addbook("Atharv 1")
+l1.addbook("Atharv 2")
+l1.addbook("Atharv 3")
+l1.show()
+        
