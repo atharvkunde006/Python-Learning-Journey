dff544a1d6c5ee9d3418e39b79d26050ccf6de58
Atharv Kunde <atharvkunde006@gmail.com>
2026-08-31 21:48:33 +0530

    Delete alaram.py

--- alaram.py
+++
@@ -1,7 +0,0 @@
-import win32com.client as win
-import time
-speaker=win.Dispatch("SAPI.Spvoice")
-time.sleep(3)
-alaram=f"tring tring tring wake up bro this is morning now"
-print(alaram)
-speaker.speak(alaram)
